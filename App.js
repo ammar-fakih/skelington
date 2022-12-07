fd89721807936ec5f7fa58859d3648f78a86dab9
Ammar Fakih <ammar.fakih02@gmail.com>
2022-12-06 19:54:42 -0800

    updated for web

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useColorScheme } from 'react-native';
+import { useColorScheme, Text } from 'react-native';
 import { useState, useEffect } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
@@ -7,23 +7,39 @@ import {
   DefaultTheme,
   DarkTheme,
 } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 import Main from './Components/Main';
 import { AppDataContext } from './contexts';
 
 export default function App() {
   const [darkMode, setDarkMode] = useState();
-  // const scheme = useColorScheme();
+  const [userData, setUserData] = useState();
+  const scheme = useColorScheme();
 
-  // useEffect(() => {
-  //   setTheme(scheme);
-  // }, []);
+  const getUserData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@user_data');
+      setUserData(jsonValue != null ? JSON.parse(jsonValue) : null);
+    } catch (e) {
+      console.warn('Get User Data', e);
+    }
+  };
+
+  useEffect(() => {
+    if (scheme === 'dark') {
+      setDarkMode(true);
+    } else {
+      setDarkMode(false);
+    }
+
+    getUserData();
+  }, []);
 
   return (
     <NavigationContainer theme={darkMode ? DarkTheme : DefaultTheme}>
-      <ApplicationProvider
-        {...eva}
-        theme={darkMode ? eva.dark : eva.light}>
+      <ApplicationProvider {...eva} theme={darkMode ? eva.dark : eva.light}>
         <AppDataContext.Provider value={{ darkMode, setDarkMode }}>
           <Main />
         </AppDataContext.Provider>
@@ -31,3 +47,4 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -2,8 +2,10 @@ import { Layout, Text } from '@ui-kitten/components';
 
 export default function HomeScreen() {
   return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text category="h2">Welcome to the Future</Text>
+    <Layout style={{ flex: 1, alignItems: 'center' }}>
+      <Layout style={{ flex: 1, marginTop: 60 }}>
+        <Text category="h2">Welcome to the Future</Text>
+      </Layout>
     </Layout>
   );
 }
